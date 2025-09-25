e1440b13b3c12fb23a34d652dd9d0781d22888be
rrvelar <123507435+rrvelar@users.noreply.github.com>
2025-09-25 19:22:24 +0500

    Delete api/index.ts

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-// api/index.ts
-import app from "../src/app";
-
-// запускаем как Edge Function (поддерживает Fetch API без обёрток)
-export const config = { runtime: "edge" };
-
-export default app.fetch;
